5d53e03c41df4505b4af25f277a55d90da5f1ff3
Anton <madrid.87@mail.ru>
2020-12-13 17:22:05 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-"use strict";
 
-alert("Hello!");
 
-const result = confirm('Are you here?');
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+const personalMovieDB = {
+
+  count: numberOfFilms,
+  movie:{},
+  actors:{},
+  genres:[],
+  privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt ('На сколько оцените его', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt ('На сколько оцените его', '');
+
+ personalMovieDB.movies[a] = b;
+ personalMovieDB.movies[c] = d;
+
+ console.log(personalMovieDB.movies);
